030cad572fbdf951ad1a9ba3b69a2ba0ad8e48be
정호심 <ghtla0330@naver.com>
2022-08-03 16:38:51 +0900

    first upload - icon fin

---
+++ src/main/java/com/example/bbosongbbo_bootstrap2/BbosongbboBootstrap2Application.java
@@ -0,0 +1,14 @@
+package com.example.bbosongbbo_bootstrap2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbosongbboBootstrap2Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BbosongbboBootstrap2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bbosongbbo_bootstrap2/BbosongbboBootstrap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bbosongbbo_bootstrap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbosongbboBootstrap2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
