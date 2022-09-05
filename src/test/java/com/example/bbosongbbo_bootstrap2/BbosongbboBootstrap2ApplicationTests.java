9511177b822b7a64da6ddb446b239c6f8b72d856
정호심 <ghtla0330@naver.com>
2022-09-05 20:32:01 +0900

    0905 build try

--- src/test/java/com/example/bbosongbbo_bootstrap2/BbosongbboBootstrap2ApplicationTests.java
+++ src/test/java/com/example/bbosongbbo_bootstrap2/BbosongbboBootstrap2ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.bbosongbbo_bootstrap2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BbosongbboBootstrap2ApplicationTests {
 
     @Test
