97aa471c4f67a49ba0bc51489c3908e81f52cdd2
정호심 <ghtla0330@naver.com>
2022-10-14 01:26:11 +0900

    1014 path, design change

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -28,7 +28,7 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             // alert('글이 등록되었습니다.');
-            window.location.href = '/';
+            window.location.href = '/tables.html';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
@@ -49,7 +49,7 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             // alert('글이 수정되었습니다.');
-            window.location.href = '/';
+            window.location.href = '/tables.html';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
@@ -65,7 +65,7 @@ var main = {
             contentType:'application/json; charset=utf-8'
         }).done(function() {
             // alert('글이 삭제되었습니다.');
-            window.location.href = '/';
+            window.location.href = '/tables.html';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
